249199be8a8d0836dbb0f34abd3ffbdd6e592db2
seonglae <sungle3737@gmail.com>
2021-08-24 12:39:37 +0900

    doc: fix repo link oriign

--- .vitepress/theme/composables/repo.ts
+++ .vitepress/theme/composables/repo.ts
@@ -2,7 +2,7 @@ import { computed } from 'vue-demi'
 import { useSiteDataByRoute } from 'vitepress'
 import type { DefaultTheme } from '../config'
 
-export const platforms = ['GitHub', 'GitLab', 'Bitbucket'].map((platform) => {
+export const platforms = ['GitHub', 'GitLab', 'Bitbucket'].map(platform => {
   return [platform, new RegExp(platform, 'i')] as const
 })
 
@@ -11,10 +11,9 @@ export function useRepo() {
 
   return computed(() => {
     const theme = site.value.themeConfig as DefaultTheme.Config
-    const name = theme.docsRepo || theme.repo
+    const name = theme.repo || theme.docsRepo
 
-    if (!name)
-      return null
+    if (!name) return null
 
     const link = getRepoUrl(name)
     const text = getRepoText(link, theme.repoLabel)
@@ -29,19 +28,16 @@ function getRepoUrl(repo: string): string {
 }
 
 function getRepoText(url: string, text?: string): string {
-  if (text)
-    return text
+  if (text) return text
 
   // if no label is provided, deduce it from the repo url
   const hosts = url.match(/^https?:\/\/[^/]+/)
 
-  if (!hosts)
-    return 'Source'
+  if (!hosts) return 'Source'
 
   const platform = platforms.find(([, re]) => re.test(hosts[0]))
 
-  if (platform && platform[0])
-    return platform[0]
+  if (platform && platform[0]) return platform[0]
 
   return 'Source'
 }
