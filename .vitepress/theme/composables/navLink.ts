1bee72e918ffa34488913e00037ecb091706f460
seonglae <sungle3737@gmail.com>
2021-09-19 01:58:06 +0900

    fix: remove url.ts file

--- .vitepress/theme/composables/navLink.ts
+++ .vitepress/theme/composables/navLink.ts
@@ -2,45 +2,27 @@ import { computed, Ref } from 'vue-demi'
 import { useRoute } from 'vitepress'
 import type { DefaultTheme } from '../config'
 import { isExternal as isExternalCheck } from '../utils'
-import { useUrl } from '../composables/url'
 
 export function useNavLink(item: Ref<DefaultTheme.NavItemWithLink>) {
   const route = useRoute()
-  const { withBase } = useUrl()
-
   const isExternal = isExternalCheck(item.value.link)
-
   const props = computed(() => {
     const routePath = normalizePath(`/${route.data.relativePath}`)
-
     let active = false
-    if (item.value.activeMatch) {
-      active = new RegExp(item.value.activeMatch).test(routePath)
-    }
+    if (item.value.activeMatch) active = new RegExp(item.value.activeMatch).test(routePath)
     else {
-      const itemPath = normalizePath(withBase(item.value.link))
-      active
-        = itemPath === '/'
-          ? itemPath === routePath
-          : routePath.startsWith(itemPath)
+      const itemPath = normalizePath(item.value.link)
+      active = itemPath === '/' ? itemPath === routePath : routePath.startsWith(itemPath)
     }
-
     return {
-      'class': {
-        active,
-        isExternal,
-      },
-      'href': isExternal ? item.value.link : withBase(item.value.link),
-      'target': item.value.target || isExternal ? '_blank' : null,
-      'rel': item.value.rel || isExternal ? 'noopener noreferrer' : null,
+      class: { active, isExternal },
+      href: isExternal ? item.value.link : item.value.link,
+      target: item.value.target || isExternal ? '_blank' : null,
+      rel: item.value.rel || isExternal ? 'noopener noreferrer' : null,
       'aria-label': item.value.ariaLabel,
     }
   })
-
-  return {
-    props,
-    isExternal,
-  }
+  return { props, isExternal }
 }
 
 function normalizePath(path: string): string {
