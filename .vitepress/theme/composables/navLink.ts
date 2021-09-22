de65752d916a319aea9b3bf13f548308281d77ee
Seonglae Cho <sungle3737@gmail.com>
2021-09-23 01:11:34 +0900

    Revert "Revert "fix: no normalize path and cp assets""

--- .vitepress/theme/composables/navLink.ts
+++ .vitepress/theme/composables/navLink.ts
@@ -8,11 +8,11 @@ export function useNavLink(item: Ref<DefaultTheme.NavItemWithLink>) {
   const isExternal = isExternalCheck(item.value.link)
 
   const props = computed(() => {
-    const routePath = normalizePath(`/${route.data.relativePath}`)
+    const routePath = `/${route.data.relativePath}`
     let active = false
     if (item.value.activeMatch) active = new RegExp(item.value.activeMatch).test(routePath)
     else {
-      const itemPath = normalizePath(withBase(item.value.link))
+      const itemPath = withBase(item.value.link)
       active = itemPath === '/' ? itemPath === routePath : routePath.startsWith(itemPath)
     }
     return {
@@ -24,12 +24,4 @@ export function useNavLink(item: Ref<DefaultTheme.NavItemWithLink>) {
     }
   })
   return { props, isExternal }
-}
-
-function normalizePath(path: string): string {
-  return path
-    .replace(/#.*$/, '')
-    .replace(/\?.*$/, '')
-    .replace(/\.(html|md)$/, '')
-    .replace(/\/index$/, '/')
 }
