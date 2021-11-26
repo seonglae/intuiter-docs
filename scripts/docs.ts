a2de03e5ec568bdf6a242e5e9d76103044ef8da8
seonglae <sungle3737@gmail.com>
2021-11-27 01:35:38 +0900

    feat: non mpa

--- scripts/docs.ts
+++ scripts/docs.ts
@@ -1,7 +1,7 @@
 import path from 'path'
 import { run } from './run'
 
-const build = async () => run('npx vitepress build  --mpa', path.resolve(__dirname, '../'))
+const build = async () => run('npx vitepress build', path.resolve(__dirname, '../'))
 
 build().catch(e => {
   console.error(e)
